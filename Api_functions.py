f45447fdb4ef184f3a4bf99aa8461b8fe2ebbdbb
daimon20 <daimon20777@gmail.com>
2017-02-16 15:12:26 +0300

    Add files via upload

--- Api_functions.py
+++ Api_functions.py
@@ -9,25 +9,25 @@ def load_json_data_from_url(base_url, url_params):
     return json.loads(response)
 
 
-def make_tmdb_api_request(method, api_key, extra_params=None):
+def make_tmdb_api_request(method, apiKey, extra_params=None):
     extra_params = extra_params or {}
     url = 'https://api.themoviedb.org/3%s' % method
     params = {
-        'api_key': api_key,
+        'apiKey': apiKey,
         'language': 'en_US',
     }
     params.update(extra_params)
     return load_json_data_from_url(url, params)
 
 
-def get_api_key():
-    api_key = input("Enter your tmdb api key:")
-    return api_key
+def getApiKey():
+    apiKey = input("Enter your api key:")
+    return apiKey
 
 
-def find_movie(movie_id, api_key=''):
-    api_key = api_key or get_api_key()
-    movie = make_tmdb_api_request("/movie/{}".format(movie_id), api_key)
+def getMovie(movieId, apiKey=''):
+    apiKey = apiKey or getApiKey()
+    movie = make_tmdb_api_request('/movie/%d' % movieId, apiKey)
     return movie
 
 
--- Budget_search.py
+++ Budget_search.py
@@ -2,13 +2,13 @@ import Api_functions
 import urllib.request
 
 if __name__ == "__main__":
-    movie_id = int(input("Enter movie_id"))
+    movieId = int(input("Enter movie_id"))
 
     try:
-        movie = Api_functions.find_movie(movie_id)
+        movie = Api_functions.getMovie(movieId)
     except urllib.request.HTTPError as error:
         if error.getcode() == 404:
-            print("Can't find the movie with ID {}".format(movie_id))
+            print("Can't find the movie with ID {}".format(movieId))
             exit(1)
         else:
             print(error.msg)
